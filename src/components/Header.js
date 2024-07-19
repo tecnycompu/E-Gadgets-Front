fe2bd5bc98004190b3e47d959848b80f04feb4f9
tecnycompu <tecnycompu@gmail.com>
2024-07-19 07:40:16 -0500

    AllProducts

--- src/components/Header.js
+++ src/components/Header.js
@@ -54,22 +54,27 @@ const Header = () => {
             <div className='flex items-center gap-7'>
                 
                 <div className='relative flex justify-center'>
-                  <div className='text-3xl cursor-pointer relative flex justify-center'onClick={()=>setMenuDisplay(preve => !preve)}>
-                    {
-                      user?.profilePic ?(
-                        <img src={user?.profilePic} className='w-10 h-10 rounded-full' alt = {user?.name} />
-                      ) : (
-                        <FaRegCircleUser/>
-                      )
-                    }
-                  </div>
+                  {
+                    user?._id && (
+                      <div className='text-3xl cursor-pointer relative flex justify-center'onClick={()=>setMenuDisplay(preve => !preve)}>
+                      {
+                        user?.profilePic ?(
+                          <img src={user?.profilePic} className='w-10 h-10 rounded-full' alt = {user?.name} />
+                        ) : (
+                          <FaRegCircleUser/>
+                        )
+                      }
+                    </div>
+                    )
+                  }
+                  
                     {
                       menuDisplay && (
                         <div className='absolute bg-white bottom-0 top-11 h-fit p2 shadow-lg rounded'>
                           <nav>
                             {
                               user?.role === ROLE.ADMIN && (
-                                <Link to={"admin-panel"}className='whitespace-nowrap hidden md:block hover:bg-slate-100 p-2' onClick={()=>setMenuDisplay(preve => !preve)}>Admin Panel</Link>
+                                <Link to={"/admin-panel/all-products"}className='whitespace-nowrap hidden md:block hover:bg-slate-100 p-2' onClick={()=>setMenuDisplay(preve => !preve)}>Admin Panel</Link>
                               )
                             }
                             
