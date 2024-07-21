fa8034b5ceb94f6a75557ab32a7d9826174cc8b6
tecnycompu <tecnycompu@gmail.com>
2024-07-21 01:13:39 -0500

    update img

--- src/helpers/imageTobase64.js
+++ src/helpers/imageTobase64.js
@@ -1,13 +1,18 @@
-const imageTobase64 = async(image) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(image);
+const imageTobase64 = async (image) => {
+    try {
+        const reader = new FileReader();
+        reader.readAsDataURL(image);
 
-    const data = await new Promise((resolve, reject) => {
-        reader.onload = () => resolve(reader.result);
-        reader.onerror = error => reject(error);
-    });
+        const data = await new Promise((resolve, reject) => {
+            reader.onload = () => resolve(reader.result);
+            reader.onerror = (error) => reject(error);
+        });
 
-    return data;
-}
+        return data;
+    } catch (error) {
+        console.error("Error converting image to base64:", error);
+        return null;
+    }
+};
 
 export default imageTobase64;
