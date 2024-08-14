d519759f716d78c9c02b15eed0d858636557f5b9
tecnycompu <tecnycompu@gmail.com>
2024-08-14 08:26:44 -0500

    Uptate 1 Zoom Image Product

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -20,6 +20,11 @@ const ProductDetails = () => {
   const productImageListLoading = new Array(4).fill(null)
   const [activeImage, setActiveImage] = useState("")
 
+  const [zoomImageCoordinate,setZoomImageCoordinate] = useState({
+      x : 0,
+      y : 0
+  })
+
   console.log("product id", params)
 
   const fetchProductDetails = async () => {
@@ -33,7 +38,7 @@ const ProductDetails = () => {
       })
     })
 
-    //setLoading(false)
+    setLoading(false)
     const dataResponse = await response.json()
 
     setData(dataResponse?.data)
@@ -48,8 +53,19 @@ const ProductDetails = () => {
 
   const handleMouseEnterProduct = (imageURL) => {
     setActiveImage(imageURL)
+  }
 
+  const handleZoomImage= (e) =>{
+    const { left , top, width , height } = e.target.getBoundingClientRect()
+    console.log("coordinate", left, top , width , height)
 
+    const x = (e.clientX - left) * width 
+    const y = (e.clientY - top) * height 
+
+    setZoomImageCoordinate({
+      x,
+      y 
+    })
   }
 
   return (
@@ -59,8 +75,23 @@ const ProductDetails = () => {
         {/*** Imagen Producto */}
         <div className='h-96 flex flex-col lg:flex-row-reverse gap-4'>
 
-          <div className='h-[300px] w-[300px] lg:h-96 lg:w-96 bg-slate-200'>
-            <img src={activeImage} className='h-full w-full object-scale-down mix-blend-multiply' />
+          <div className='h-[300px] w-[300px] lg:h-96 lg:w-96 bg-slate-200 relative'>
+            <img src={activeImage} className='h-full w-full object-scale-down mix-blend-multiply' onMouseEnter={handleZoomImage}/>
+
+            {/** Imagen Zoom de Productos */}
+            <div className='hidden lg:block absolute min-w-[400px] min-h-[400px] bg-slate-200 p-1 -right-[410px] top-0'>
+              <div
+                className='w-full h-full min-h-[400px] min-w-[400px] mix-blend-multiply'
+                style={{
+                  backgroundImage : `url(${activeImage})`,
+                  backgroundRepeat : 'no-repeat',
+                  backgroundPosition : `${zoomImageCoordinate.x *100}% ${zoomImageCoordinate.y *100}%`
+                }}
+              >
+
+              </div>
+            </div>
+
           </div>
 
           <div className='h-full'>
@@ -100,28 +131,28 @@ const ProductDetails = () => {
         {/*** Detalles Producto */}
         {
           loading ? (
-            <div className='flex flex-col gap-1'>
-              <p className='bg-slate-200 animate-pulse h-6 w-full rounded-full inline-block'></p>
-              <h2 className='text-2xl lg:text-4xl font-medium h-6 bg-slate-200 animate-pulse'></h2>
-              <p className='capitalize text-slate-400 bg-slate-200 min-w-[100px] animate-pulse h-6'></p>
+            <div className='grid gap-1 w-full'>
+              <p className='bg-slate-200 animate-pulse h-6 lg:h-8 w-full rounded-full inline-block'></p>
+              <h2 className='text-2xl lg:text-4xl font-medium h-6 lg:h-8  bg-slate-200 animate-pulse w-full'></h2>
+              <p className='capitalize text-slate-400 bg-slate-200 min-w-[100px] animate-pulse h-6 lg:h-8 w-full'></p>
 
-              <div className='text-red-600 bg-slate-200 h-6 animate-pulse flex items-center gap-1 '>
+              <div className='text-red-600 bg-slate-200 h-6 lg:h-8 animate-pulse flex items-center gap-1 w-full'>
             
               </div>
 
-              <div className='flex items-center gap-2 text-2xl lg:text-3xl font-medium my-1'>
-                <p className='text-red-600'>{displayINRCurrency(data.sellingPrice)}</p>
-                <p className='text-slate-400 line-through'>{displayINRCurrency(data.price)}</p>
+              <div className='flex items-center gap-2 text-2xl lg:text-3xl font-medium my-1 h-6 lg:h-8 animate-pulse w-full'>
+                <p className='text-red-600 bg-slate-200 w-full'></p>
+                <p className='text-slate-400 line-through bg-slate-200 w-full'></p>
               </div>
 
-              <div className='flex items-center gap-3 my-2'>
-                <button className='border-2 border-red-600 rounded px-3 py-1 min-w-[120px] text-red-600 font-medium hover:bg-red-600 hover:text-white '>Comprar</button>
-                <button className='border-2 border-red-600 rounded px-3 py-1 min-w-[120px] font-medium text-white bg-red-600 hover:text-red-600 hover:bg-white'>Agregar al Carro</button>
+              <div className='flex items-center gap-3 my-2 w-full'>
+                <button className='h-6 lg:h-8 bg-slate-200 rounded animate-pulse w-full'></button>
+                <button className='h-6 lg:h-8 bg-slate-200 rounded animate-pulse w-full'></button>
               </div>
 
-              <div>
-                <p className='text-slate-500 font-medium my-1'>Descripción : </p>
-                <p>{data?.description}</p>
+              <div className='w-full'>
+                <p className='text-slate-500 font-medium my-1 h-6 lg:h-8 bg-slate-200 rounded animate-pulse w-full'></p>
+                <p className='bg-slate-200 rounded animate-pulse h-10  lg:h-12 w-full'></p>
               </div>
 
             </div>
